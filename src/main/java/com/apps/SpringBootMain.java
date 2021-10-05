90db03f5297b87e862a92c2b3538004d2b18de5f
Cholo Asis <cholo_asis@bitbucket.org>
2021-10-05 17:18:46 +0800

    back 2

---
+++ src/main/java/com/apps/SpringBootMain.java
@@ -0,0 +1,27 @@
+package com.apps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+
+@SpringBootApplication
+public class SpringBootMain {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootMain.class, args);
+    }
+
+    @EventListener({ ApplicationReadyEvent.class })
+    public void applicationReadyEvent() {
+
+        // Add code here that will run when the spring boot app completed initializing.
+        System.out.println("****************************************************");
+        System.out.println("* STARTING YOUR APPLICATION ...                    *");
+        System.out.println("****************************************************");
+        
+        //some changes 1
+        //some changes 2
+        //some changes 3
+    }
+}
